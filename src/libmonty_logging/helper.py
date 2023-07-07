08e2a2ae7eb31dad340cec55d9a56f22ba433c5b
András Németh <sunarch@protonmail.com>
2023-07-07 20:33:36 +0200

    Remove editor config lines from files

--- src/libmonty_logging/helper.py
+++ src/libmonty_logging/helper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -31,6 +30,7 @@ def log_file_path(program_name: str, program_version: str) -> str:
     state_dir_path: str = _state_dir_path(program_name)
     log_file_name: str = _log_file_name(program_name, program_version)
     return os.path.join(state_dir_path, log_file_name)
+
 
 def apply_config(program_name: str, program_version: str, configurator: Callable) -> None:
     config = configurator(program_name, program_version)
