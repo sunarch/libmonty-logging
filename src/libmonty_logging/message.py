decd77c6b2e82861baa471d36774ae1d06ed995d
András Németh <sunarch@protonmail.com>
2024-06-15 17:35:24 +0200

    Remove hashbang lines

--- src/libmonty_logging/message.py
+++ src/libmonty_logging/message.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -12,12 +10,12 @@ def program_header(program_name: str,
                    program_version: str = None,
                    underliner: str = '-'
                    ) -> None:
-
     if len(underliner) != 1:
         raise ValueError(f'Argument "underliner" must be 1 character long: "{underliner}"')
 
+
     header = f'{program_name}'
-    
+
     if program_version is not None:
         header += f' {program_version}'
 
